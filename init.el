be1a5a514db3781a7145ff714b681f4146a02101
Volkov Alexander <volkov.525@gmail.com>
2020-06-20 17:31:58 +0300

    add vue js tab config

--- init.el
+++ init.el
@@ -14,7 +14,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(ranger git-gutter evil-magit magit avy gruvbox-theme org)))
+ '(package-selected-packages
+   '(company ranger git-gutter evil-magit magit avy gruvbox-theme org)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
