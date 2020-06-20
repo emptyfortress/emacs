3120265c756ffdda2640cd7cb81957627b440497
Volkov Alexander <volkov.525@gmail.com>
2020-06-20 17:56:53 +0300

    change feedback color on evil-goggles

--- init.el
+++ init.el
@@ -21,4 +21,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(evil-goggles-change-face ((t (:inherit diff-removed))))
+ '(evil-goggles-delete-face ((t (:inherit diff-removed))))
+ '(evil-goggles-paste-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
+ '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
+ '(evil-goggles-yank-face ((t (:inherit region))))
+ '(org-scheduled-previously ((t (:foreground "#ff00cc"))))
+ '(region ((t (:background "#94765A"))))
+ '(secondary-selection ((t (:background "#836D62" :foreground "black")))))
