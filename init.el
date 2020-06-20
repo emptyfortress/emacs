00fc69389bc3dc5bb85d4888a18eb13c310e0553
Volkov Alexander <volkov.525@gmail.com>
2020-06-20 16:23:08 +0300

    add some staff and yasnippet

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil-magit magit avy gruvbox-theme org)))
+ '(package-selected-packages '(ranger git-gutter evil-magit magit avy gruvbox-theme org)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
