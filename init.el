8f7749dff8c5afd3aeb1b5f0476bf14aa455aab2
Volkov Alexander <volkov.525@gmail.com>
2020-06-20 18:33:56 +0300

    add ace-window

--- init.el
+++ init.el
@@ -14,8 +14,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" default))
  '(package-selected-packages
-   '(company ranger git-gutter evil-magit magit avy gruvbox-theme org)))
+   '(ace-window expand-region company ranger git-gutter evil-magit magit avy gruvbox-theme org)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
