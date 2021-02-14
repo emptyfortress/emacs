97b4d889679992f650f7110245149183acd7c7ea
Volkov Alexander <volkov.525@gmail.com>
2021-02-14 15:24:27 +0300

    return original files, kill symlink = i haver repository for emacs

--- init.el
+++ init.el
@@ -1,1 +1,38 @@
-/Users/admin/dotfiles/init.el
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(require 'package)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+
+(require 'org)
+(org-babel-load-file (expand-file-name "~/.emacs.d/settings.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" default)))
+ '(org-roam-directory "~/Dropbox/org")
+ '(package-selected-packages
+   (quote
+    (csv-mode typescript-mode js2-mode evil-vimish-fold vimish-fold lorem-ipsum pug-mode web-mode anki-editor org-download ace-window expand-region company ranger git-gutter evil-magit magit avy gruvbox-theme org))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-goggles-change-face ((t (:inherit diff-removed))))
+ '(evil-goggles-delete-face ((t (:inherit diff-removed))))
+ '(evil-goggles-paste-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
+ '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
+ '(evil-goggles-yank-face ((t (:inherit region))))
+ '(org-scheduled-previously ((t (:foreground "#ff00cc"))))
+ '(region ((t (:background "#94765A"))))
+ '(secondary-selection ((t (:background "#836D62" :foreground "black")))))
